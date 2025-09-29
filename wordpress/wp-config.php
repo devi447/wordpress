a9c1e283f504fcbf6f6d580518d41de12ce36efe
devi447 <devisingh447@gmail.com.com>
2025-09-29 09:31:40 +0530

    test

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -27,7 +27,7 @@ define( 'DB_NAME', 'wordpress_db' );
 define( 'DB_USER', 'admin' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'TechTest$12345' );
+define( 'DB_PASSWORD', 'TechTest$1234' );
 
 /** Database hostname */
 define( 'DB_HOST', 'db-mysql-rds.cpowkocggejt.us-east-2.rds.amazonaws.com' );
